439f164858121894ac45b100eb1915e31718db29
Bucciarelli Alessio <alessiobuccia@gmail.com>
2018-11-08 17:01:09 +0100

    _risolti problemi inerenti alla ugando-guardia_

--- IsTimeToStop/Assets/_Prommamming/Scripts/Enemy/MainEnemy.cs
+++ IsTimeToStop/Assets/_Prommamming/Scripts/Enemy/MainEnemy.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class MainEnemy : MonoBehaviour
 {
+    [HideInInspector]public GameObject visionArea;
     public Sprite redX;
     public float movDistance = 5f;
     public float movVelocity = 1f;
@@ -11,43 +12,93 @@ public class MainEnemy : MonoBehaviour
     private int direction = -1;
     private float startingposX;
     private float newPos;
+    private GameObject player;
+    private GameObject gameManager;
+    private bool isLock = false;
+
 
     private void Start()
     {
+        player = GameObject.Find("Player");
+        player.GetComponent<PlayerAction>().ZaWarudo += LockMyself;
+        player.GetComponent<PlayerAction>().StopZaWarudo += UnlockMyself;
+        gameManager = GameObject.Find("Game_Manager");
+
         startingposX = gameObject.transform.position.x;
         newPos = startingposX;
     }
 
     void Update()
     {
-        switch (direction)
+        if(!isLock)
         {
-            case -1:
-                // Moving Left
-                if (newPos > startingposX - movDistance)
-                {
-                    newPos -= movVelocity;
-                }
-                else
-                {
-                    direction = 1;
-                }
-                break;
+            switch (direction)
+            {
+                case -1:
+                    // Moving Left
+                    if (newPos > startingposX - movDistance)
+                    {
+                        newPos -= movVelocity;
+                    }
+                    else
+                    {
+                        direction = 1;
+                    }
+                    break;
 
-            case 1:
-                // Moving Right
-                if (newPos < startingposX + movDistance)
-                {
-                    newPos += movVelocity;
-                }
-                else
-                {
-                    direction = -1;
-                }
-                break;
+                case 1:
+                    // Moving Right
+                    if (newPos < startingposX + movDistance)
+                    {
+                        newPos += movVelocity;
+                    }
+                    else
+                    {
+                        direction = -1;
+                    }
+                    break;
+            }
+
+            gameObject.transform.localPosition = new Vector3(newPos, gameObject.transform.position.y, gameObject.transform.position.z);
         }
+        
+    }
 
-        gameObject.transform.localPosition = new Vector3(newPos, gameObject.transform.position.y, gameObject.transform.position.z);
+    private void OnDestroy()
+    {
+        if (player != null)
+        {
+            player.GetComponent<PlayerAction>().ZaWarudo -= LockMyself;
+            player.GetComponent<PlayerAction>().StopZaWarudo -= UnlockMyself;
+        }
+    }
+
+
+    public void LockMyself()
+    {
+        if (!isLock)
+        {
+            isLock = true;
+            visionArea.SetActive(false);
+            if (gameObject.GetComponent<Rigidbody2D>())
+            {
+                gameObject.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeAll;
+            }
+        }
+    }
+
+    public void UnlockMyself()
+    {
+        if (isLock)
+        {
+            isLock = false;
+            visionArea.SetActive(true);
+            if (gameObject.GetComponent<Rigidbody2D>())
+            {
+                gameObject.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.None;
+                gameObject.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeRotation;
+            }
+        }
     }
 
 }
