0e5ba07d01f7047d14a838c2233605d0ee998136
Bucciarelli Alessio <alessiobuccia@gmail.com>
2018-11-08 17:30:11 +0100

    _ aggiunto un manager per la musica in background _

---
+++ IsTimeToStop/Assets/_Prommamming/Scripts/GameManager/MusicManager/MusicManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+    public List<AudioClip> background;
+    public int musicBackgroundSelector = 1;
+    [HideInInspector]public GameObject musicManager;
+    
+	void Start ()
+    {
+        musicManager.GetComponent<AudioSource>().clip = background[musicBackgroundSelector -1];
+        musicManager.GetComponent<AudioSource>().Play();
+    }
+	
+	void Update ()
+    {
+		
+	}
+}
